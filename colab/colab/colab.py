d3a6eebac4b54f2d3439d73bb71e47d747b31210
Christian <chris.busch.22@gmail.com>
2019-10-29 21:15:41 +0100

    dataset test

--- colab/colab/colab.py
+++ colab/colab/colab.py
@@ -30,14 +30,12 @@ def get_repository_settings_private(settings):
 def upload_dataset():
     uploaded = files.upload()
     dataset_name_zip = list(uploaded.keys())[0]
-    path_dataset_zip = os.path.join('content', dataset_name_zip)
+    dataset_name = os.path.splitext(dataset_name_zip)[0]
 
-    dataset_name = os.path.splitext(path_dataset_zip)[0]
-
-    zip_ = zipfile.ZipFile(path_dataset_zip, 'r')
-    zip_.extractall(dataset_name)
+    zip_ = zipfile.ZipFile(dataset_name_zip, 'r')
+    zip_.extractall('dataset')
     zip_.close()
 
-    dataset_path = os.path.join(['content', dataset_name, dataset_name])
+    dataset_path = os.path.join('dataset', dataset_name)
 
     return dataset_name, dataset_path
