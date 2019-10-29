51e3aa1b8b15714e0f036d83eebeee838b09ecef
Christian <chris.busch.22@gmail.com>
2019-10-29 20:55:31 +0100

    upload_dataset.

--- colab/colab/colab.py
+++ colab/colab/colab.py
@@ -1,10 +1,14 @@
+import os
 import yaml
+import zipfile
+
+from google.colab import drive
+from google.colab import files
 
 def hello_colab():
     print('Hello colab!')
 
 def mount_drive(path_drive='/content/drive'):
-    from google.colab import drive
     drive.mount(path_drive)
 
 def get_settings(path_file):
@@ -22,3 +26,12 @@ def get_repository_settings_private(settings):
     GIT_REPOSITORY = settings['git']['private']['repository']
     PATH_CODE = settings['git']['private']['path']
     return GIT_USERNAME, GIT_TOKEN, GIT_REPOSITORY, PATH_CODE
+
+def upload_dataset():
+    uploaded = files.upload()
+    dataset_name = list(uploaded.keys())[0]
+    path_dataset = os.path.join('content', dataset_name)
+
+    zip_ = zipfile.ZipFile(dataset_name, 'r')
+    zip_.extractall(os.path.splitext(path_dataset)[0])
+    zip_.close()
